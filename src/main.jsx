f088870c08f259f2c269c48c797c356fc68b4880
Mika <test@Tests-MacBook-Pro.local>
2024-10-13 16:27:12 -0300

    feat: install redux dependencies and set up initial configuration
    - installed redux and redux-related dependencies
    - added initial redux store setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import store from "./redux";
+
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
