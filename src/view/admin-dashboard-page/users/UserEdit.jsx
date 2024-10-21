c54c8c08bb44bc6526b4d60fa516d75e9ef9e4c3
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-10-20 21:04:04 -0300

    feat user edit working

--- src/view/admin-dashboard-page/users/UserEdit.jsx
+++ src/view/admin-dashboard-page/users/UserEdit.jsx
@@ -1,28 +1,11 @@
-import { Edit, SimpleForm, TextInput, useNotify, useRefresh, useRedirect } from 'react-admin';
-import { useDataProvider } from 'react-admin';
+import { Edit, SimpleForm, TextInput } from 'react-admin';
 
 const required = (value) => (value ? undefined : 'El campo es obligatorio');
 
 const UserEdit = (props) => {
-  const notify = useNotify();
-  const refresh = useRefresh();
-  const redirect = useRedirect();
-  const dataProvider = useDataProvider();
-
-  const handleEdit = async (data) => {
-    try {
-      await dataProvider.update('users', { id: data.id, data });
-      notify('Usuario actualizado con éxito');
-      redirect('/admin/users');
-      refresh();
-    } catch (error) {
-      notify(`Error: ${error.message}`, { type: 'warning' });
-    }
-  };
-
   return (
     <Edit {...props}>
-      <SimpleForm onSubmit={handleEdit}>
+      <SimpleForm>
         <TextInput source="username" label="Nombre de Usuario" validate={required} />
         <TextInput source="email" label="Correo Electrónico" validate={required} />
       </SimpleForm>
