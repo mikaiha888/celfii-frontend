3583fe6b5377985177bef9affe6d9e9cad8bd2c0
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-10-23 18:32:56 -0300

    feat remove edit and remove button from deleted products

--- src/view/admin-dashboard-page/products/ProductList.jsx
+++ src/view/admin-dashboard-page/products/ProductList.jsx
@@ -8,23 +8,35 @@ import {
   DeleteButton,
   CreateButton,
   ExportButton,
+  useListContext,
 } from 'react-admin';
 import { ProductFilterSidebar } from './ProductFilterSidebar';
 
-export const ProductList = (props) => (
-  <List
-    title="Productos"
-    {...props}
-    perPage={25}
-    rowsPerPageLabel="Filas por página"
-    aside={<ProductFilterSidebar />}
-    actions={
-      <div >
-        <CreateButton label="Crear Producto" />
-        <ExportButton label="Exportar Productos" />
-      </div>
-    }
-  >
+export const ProductList = (props) => {
+  return (
+    <List
+      title="Productos"
+      {...props}
+      perPage={25}
+      rowsPerPageLabel="Filas por página"
+      aside={<ProductFilterSidebar />}
+      actions={
+        <div>
+          <CreateButton label="Crear Producto" />
+          <ExportButton label="Exportar Productos" />
+        </div>
+      }
+    >
+      <ProductDataGrid />
+    </List>
+  );
+};
+
+const ProductDataGrid = () => {
+  const { filterValues } = useListContext();
+  const showDeleted = filterValues.onlyDeleted;
+
+  return (
     <Datagrid>
       <TextField source="name" label="Nombre del Producto" />
       <ImageField source="images[0].url" label="Imagen" />
@@ -33,10 +45,15 @@ export const ProductList = (props) => (
       <NumberField source="stock" label="Stock" />
       <TextField source="code" label="Código" />
       <NumberField source="view.counter" label="Vistas" />
-      <EditButton label="Editar" />
-      <DeleteButton label="Eliminar" />
+
+      {!showDeleted && (
+        <>
+          <EditButton label="Editar" />
+          <DeleteButton label="Eliminar" />
+        </>
+      )}
     </Datagrid>
-  </List>
-);
+  );
+};
 
 export default ProductList;
