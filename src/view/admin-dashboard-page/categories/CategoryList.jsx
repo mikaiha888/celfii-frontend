113cd90afa3c6fbddb44194fb848c09df151939c
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-10-23 08:14:10 -0300

    feat Filter sideBarComponnet

--- src/view/admin-dashboard-page/categories/CategoryList.jsx
+++ src/view/admin-dashboard-page/categories/CategoryList.jsx
@@ -1,13 +1,22 @@
-import { List, Datagrid, TextField, EditButton, DeleteButton } from 'react-admin';
+import { List, Datagrid, TextField, EditButton, DeleteButton, CreateButton, ExportButton } from 'react-admin';
 
 export const CategoryList = () => {
   return (
     <div>
-      <List title="Categorias" pagination={false}>
+      <List
+        title="Categorias"
+        pagination={false}
+        actions={
+          <div>
+            <CreateButton label="Crear Categoria" />
+            <ExportButton label="Exportar Categorias" />
+          </div>
+        }
+      >
         <Datagrid>
           <TextField source="name" label="Nombre de la Categoría" />
           <EditButton label="Editar" />
-          <DeleteButton label="Eliminar"  />
+          <DeleteButton label="Eliminar" />
         </Datagrid>
       </List>
     </div>
