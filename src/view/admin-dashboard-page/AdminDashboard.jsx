aefe22ca63af46b7b10a0b61f2b2d2aaa647073c
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-10-24 18:29:16 -0300

    feat Category Chart

--- src/view/admin-dashboard-page/AdminDashboard.jsx
+++ src/view/admin-dashboard-page/AdminDashboard.jsx
@@ -1,23 +1,25 @@
-import dataProvider from "./dataProvider";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import { Admin, Resource, Layout, AppBar, UserMenu } from "react-admin";
-import { useNavigate } from "react-router-dom";
-import { Button } from "@mui/material";
+import dataProvider from './dataProvider';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import { Admin, Resource, Layout, AppBar, UserMenu } from 'react-admin';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
-import ProductList from "../../components/admin/products/ProductList";
-import ProductShow from "../../components/admin/products/ProductShow";
-import ProductCreate from "../../components/admin/products/ProductCreate";
-import ProductEdit from "../../components/admin/products/ProductEdit";
+import ProductList from '../../components/admin/products/ProductList';
+import ProductShow from '../../components/admin/products/ProductShow';
+import ProductCreate from '../../components/admin/products/ProductCreate';
+import ProductEdit from '../../components/admin/products/ProductEdit';
 
-import UserList from "../../components/admin/users/UserList";
-import UserCreate from "../../components/admin/users/UserCreate";
-import UserEdit from "../../components/admin/users/UserEdit";
+import UserList from '../../components/admin/users/UserList';
+import UserCreate from '../../components/admin/users/UserCreate';
+import UserEdit from '../../components/admin/users/UserEdit';
 
-import CategoryList from "../../components/admin/categories/CategoryList";
-import CategoryCreate from "../../components/admin/categories/CategoryCreate";
-import CategoryEdit from "../../components/admin/categories/CategoryEdit";
+import CategoryList from '../../components/admin/categories/CategoryList';
+import CategoryCreate from '../../components/admin/categories/CategoryCreate';
+import CategoryEdit from '../../components/admin/categories/CategoryEdit';
 
-import RoleList from "../../components/admin/roles/RoleList";
+import RoleList from '../../components/admin/roles/RoleList';
+
+import Charts from '../../components/admin/charts/Charts';
 
 const CustomAppBar = (props) => {
   const navigate = useNavigate();
@@ -29,7 +31,7 @@ const CustomAppBar = (props) => {
         color="inherit"
         startIcon={<ExitToAppIcon />}
         onClick={() => {
-          navigate("/");
+          navigate('/');
         }}
       >
         Salir
@@ -44,7 +46,7 @@ const AdminDashboard = () => (
   <Admin dataProvider={dataProvider} basename="/admin" layout={CustomLayout}>
     <Resource
       name="products"
-      options={{ label: "Productos" }}
+      options={{ label: 'Productos' }}
       list={ProductList}
       show={ProductShow}
       edit={ProductEdit}
@@ -52,18 +54,23 @@ const AdminDashboard = () => (
     />
     <Resource
       name="categories"
-      options={{ label: "Categorias" }}
+      options={{ label: 'Categorias' }}
       list={CategoryList}
       edit={CategoryEdit}
       create={CategoryCreate}
     />
-    <Resource name="roles" options={{ label: "Roles" }} list={RoleList} />
+    <Resource name="roles" options={{ label: 'Roles' }} list={RoleList} />
     <Resource
       name="users"
-      options={{ label: "Usuarios" }}
+      options={{ label: 'Usuarios' }}
       list={UserList}
       create={UserCreate}
       edit={UserEdit}
+    />
+    <Resource
+      name="charts" 
+      options={{ label: 'Gráficos' }} 
+      list={Charts} 
     />
   </Admin>
 );
