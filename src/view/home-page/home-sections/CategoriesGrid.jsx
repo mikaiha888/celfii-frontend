bcd1b406e988d1447aa440f80a37ca8453445c8b
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-10-31 15:23:11 -0300

    refactor CategoriesGrid to relocate to products with filter at localStorage

--- src/view/home-page/home-sections/CategoriesGrid.jsx
+++ src/view/home-page/home-sections/CategoriesGrid.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
-import { categories } from "../contants";
 
-const CategoriesGrid = () => (
+const CategoriesGrid = ({ categories }) => (
   <section className="container flex justify-between mt-20">
     {categories.map((category) => (
-      <Link key={category.name} to={category.link}>
+      <Link
+        key={category.name}
+        to={`/productos?category=${encodeURIComponent(category.name)}`}
+        onClick={() => {
+          localStorage.setItem("selectedCategory", category.name);
+          window.scrollTo(0, 0);
+        }}
+      >
         <div className="text-center transition-transform duration-300 hover:text-primary hover:scale-105">
           <img
             src={category.image}
