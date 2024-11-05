55a93a20acf3d69a2a0d81bdf2a6dad444d17763
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-11-05 17:02:32 -0300

    feat cart button with numbers

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import { Heart, ShoppingCart, Menu } from "lucide-react";
+import { Heart, Menu } from "lucide-react";
+import CartButton from "../buttons/CartButton";
 
 import logo from "../../assets/logo-celfii2.png";
 import OptionalNavBar from "./OptionalNavBar";
@@ -37,12 +38,13 @@ const NavBar = () => {
           </ul>
         </div>
         <div className="flex items-center gap-4">
-          <Link to="/favourites" className="p-2 text-2xl duration-200 rounded-full hover:bg-primary hover:text-white">
+          <Link
+            to="/favourites"
+            className="p-2 text-2xl duration-200 rounded-full hover:bg-primary hover:text-white"
+          >
             <Heart />
           </Link>
-          <Link to="/cart" className="p-2 text-2xl duration-200 rounded-full hover:bg-primary hover:text-white">
-            <ShoppingCart />
-          </Link>
+          <CartButton />
         </div>
       </div>
       <OptionalNavBar />
