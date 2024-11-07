227906895dc3d7705f3820692bbe6d2b7467ab42
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-11-07 20:02:24 -0300

    fix get token from sessionStorage

--- src/helpers/authHelper.js
+++ src/helpers/authHelper.js
@@ -1,12 +1,12 @@
 export const getAuthHeaders = () => {
-  const token = localStorage.getItem("token");
+  const token = sessionStorage.getItem("token");
   return {
     Authorization: `Bearer ${JSON.parse(token)}`,
   };
 };
 
 export const getAuthHeadersForUsers = () => {
-  const token = localStorage.getItem("token");
+  const token = sessionStorage.getItem("token");
   return {
     Authorization: `Bearer ${JSON.parse(token)}`,
     "Content-Type": "application/json",
