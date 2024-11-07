2b58013c00db7b105cfe0adc4e155d6e22a82a95
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-11-07 20:54:05 -0300

    feat isRowSelectable={() => false} to avoid delete rows

--- src/components/admin/dollar/DollarList.jsx
+++ src/components/admin/dollar/DollarList.jsx
@@ -2,7 +2,8 @@ import { List, Datagrid, TextField, EditButton, DateField } from "react-admin";
 
 const DollarList = () => (
   <List title="Dólar" pagination={false} actions={false}>
-    <Datagrid>
+    <Datagrid isRowSelectable={() => false} >
+
       <TextField source="id" label="ID" sortable={false} />
       <TextField source="rate" label="Valor del dólar" sortable={false} />
       <DateField source="updatedAt" label="Última actualización" showTime sortable={false} />
--- src/components/admin/roles/RoleList.jsx
+++ src/components/admin/roles/RoleList.jsx
@@ -2,7 +2,7 @@ import { List, Datagrid, TextField } from "react-admin";
 
 const RoleList = () => (
   <List title="Roles" pagination={false} actions={false}>
-    <Datagrid>
+    <Datagrid isRowSelectable={() => false}>
       <TextField source="name" label="Role Name" sortable={false} />
     </Datagrid>
   </List>
